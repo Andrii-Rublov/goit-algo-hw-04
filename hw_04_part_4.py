ebb58b27852d6d249ea086d70e22f3aad7f81497
Andrii-Rublov <rublyovandrew@gmail.com>
2024-12-05 22:55:47 +0200

    comments

--- hw_04_part_4.py
+++ hw_04_part_4.py
@@ -3,24 +3,24 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
-def add_contact(args, contacts):
+def add_contact(args, contacts): # adding name and phone number to dictionare contacts
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
-def change_contact(args, contacts):
+def change_contact(args, contacts): # rewriting phone number for specified contact
     name, phone = args
     contacts[name] = phone
     return "Contact updated."
 
-def show_phone(args, contacts):
+def show_phone(args, contacts):# getting phone number for specified contact
     name = args[0]
     if name in contacts:
         return contacts[name]
     else:
         return f"Contact with name {name} not found."
 
-def show_all(_, contacts):
+def show_all(_, contacts):# shows all items in dictionary contacts
     return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
 def main():
